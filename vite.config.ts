aa1e354882724bc9bf0c8ff455b4af0e9facf529
Gigantte21 <ricoslim@hotmail.com>
2026-03-28 11:01:01 -0300

    fix base relative

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/precificaatelieprodu-o/',
+    base: './',
 
     plugins: [
       react(),
